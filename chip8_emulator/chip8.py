055ce7aeec08d7de72117cbc54cfe746e7191596
Julen Pardo <julen.pardo@outlook.es>
2018-11-30 15:38:48 +0100

    Add rest of opcodes and corresponding failing tests

--- chip8_emulator/chip8.py
+++ chip8_emulator/chip8.py
@@ -22,35 +22,138 @@ class Chip8:
         self.v_registers = []
         self.i_register = None
 
+    def clear_screen(self):
+        """ 00E0"""
+        pass
+
     def ret(self):
+        """ 00EE """
         pass
 
     def jump(self, address):
+        """ 1NNN """
         pass
 
     def call(self, address):
+        """ 2NNN """
         pass
 
     def skip_next_instruction_if_equals(self, v_index, instruction):
+        """ 3XNN """
         pass
 
     def skip_next_instruction_if_not_equals(self, v_index, instruction):
+        """ 4XNN """
+        pass
+
+    def skip_next_instruction_if_vx_equals_vy(self, vx_index, vy_index):
+        """ 5XY0 """
         pass
 
     def set_v_fixed_value(self, v_index, value):
+        """ 6XNN """
         pass
 
     def add_to_v(self, v_index, value):
+        """ 7XNN """
         pass
 
     def set_vx_to_vy(self, vx_index, vy_index):
+        """ 8XY0 """
         pass
 
     def vx_or_vy(self, vx_index, vy_index):
+        """ 8XY1 """
         pass
 
     def vx_and_vy(self, vx_index, vy_index):
+        """ 8XY2 """
         pass
 
     def vx_xor_vy(self, vx_index, vy_index):
+        """ 8XY3 """
+        pass
+
+    def add_vy_to_vx(self, vx_index, vy_index):
+        """ 8XY4 """
+        pass
+
+    def subtract_vy_from_vx(self, vx_index, vy_index):
+        """ 8XY5 """
+        pass
+
+    def shift_right_vx(self):
+        """ 8XY6 """
+        pass
+
+    def subtract_vy_from_vx_negative(self, vx_index, vy_index):
+        """ 8XY7 """
+        pass
+
+    def shift_left_vx(self):
+        """ 8XYE """
+        pass
+
+    def skip_next_instruction_if_vx_not_equals_vy(self, vx_index, vy_index):
+        """ 9XY0 """
+        pass
+
+    def set_i_register(self, value):
+        """ ANNN """
+        pass
+
+    def jump_to_address_plus_v0(self, address):
+        """ BNNN """
+        pass
+
+    def set_vx_to_random_anded_value(self, value):
+        """ CXNN """
+        pass
+
+    def draw_sprite_at_coordinate(self, vx_coord, vy_coord, sprite_height):
+        """ DXYN """
+        pass
+
+    def skip_next_instruction_if_pressed_key_equals_vx(self):
+        """ EX9E """
+        pass
+
+    def skip_next_instruction_if_pressed_key_not_equals_vx(self):
+        """ EXA1 """
+        pass
+
+    def set_vx_to_delay_timer(self, vx_index):
+        """ FX07"""
+        pass
+
+    def wait_for_key_store_value_in_vx(self, vx_index):
+        """ FX0A """
+        pass
+
+    def set_delay_timer_to_vx(self, vx_index):
+        """ FX15 """
+        pass
+
+    def set_sound_timer_to_vx(self, vx_index):
+        """ FX18 """
+        pass
+
+    def add_vx_to_i_register(self, vx_index):
+        """ FX1E """
+        pass
+
+    def set_i_register_to_location_of_sprite_for_vx(self, vx_index):
+        """ FX29 """
+        pass
+
+    def store_bcd_of_vx_in_i_register(self, vx_index):
+        """ FX33 """
+        pass
+
+    def store_from_v0_to_vx_in_i_register(self, vx_index):
+        """ FX55 """
+        pass
+
+    def read_from_v0_to_vx_in_i_register(self, vx_index):
+        """ Fx65"""
         pass
--- tests/chip8_test.py
+++ tests/chip8_test.py
@@ -14,7 +14,12 @@ class Chip8Test(unittest.TestCase):
 
         return chip8
 
+    def test_clear_screen(self):
+        """ 00E0"""
+        self.fail('Not yet tested')
+
     def test_ret(self):
+        """ 00EE """
         program_counter = 0x317
         stack_pointer = 0xEA2
         stack = [0x201, 0x204, 0x25F]
@@ -31,6 +36,7 @@ class Chip8Test(unittest.TestCase):
         self.assertEqual(expected_stack_pointer, actual_stack_pointer)
 
     def test_jump(self):
+        """ 1NNN """
         jump_to = 0x47F
 
         chip8 = self._init_chip8()
@@ -42,6 +48,7 @@ class Chip8Test(unittest.TestCase):
         self.assertEqual(expected, actual)
 
     def test_call(self):
+        """ 2NNN """
         program_counter = 0x617
         stack_pointer = 0xEA3
         stack = [0x351, 0x215, 0x812, 0x4FA]
@@ -62,6 +69,7 @@ class Chip8Test(unittest.TestCase):
         self.assertEqual(expected_stack, actual_stack)
 
     def test_skip_next_instruction_if_equals(self):
+        """ 3XNN """
         program_counter = 0x31A
         v_registers = [None] * 16
         vx_index = 0x5
@@ -77,6 +85,7 @@ class Chip8Test(unittest.TestCase):
         self.assertEqual(expected_program_counter, actual_program_counter)
 
     def test_skip_next_instructions_if_not_equals(self):
+        """ 4XNN """
         program_counter = 0xBAE
         v_registers = [None] * 16
         vx_index = 0x2
@@ -91,7 +100,12 @@ class Chip8Test(unittest.TestCase):
 
         self.assertEqual(expected_program_counter, actual_program_counter)
 
+    def test_skip_next_instruction_if_vx_equals_vy(self):
+        """ 5XY0 """
+        self.fail('Not yet tested')
+
     def test_set_v_fixed_value(self):
+        """ 6XNN """
         v_registers = [None] * 16
         vx_index = 0xA
         value = 0xEA
@@ -105,6 +119,7 @@ class Chip8Test(unittest.TestCase):
         self.assertEqual(expected_vx, actual_vx)
 
     def test_add_to_v(self):
+        """ 7XNN """
         vx_index = 0x5
         v_registers = [None] * 16
         v_registers[vx_index] = 0x78
@@ -119,6 +134,7 @@ class Chip8Test(unittest.TestCase):
         self.assertEqual(expected_vx_value, actual_vx_value)
 
     def test_set_vx_to_vy(self):
+        """ 8XY0 """
         vx_index = 0x2
         vy_index = 0x4
         v_registers = [None] * 16
@@ -134,6 +150,7 @@ class Chip8Test(unittest.TestCase):
         self.assertEqual(expected_vx_value, actual_vx_value)
 
     def test_vx_or_vy(self):
+        """ 8XY1 """
         vx_index = 0x2
         vy_index = 0x4
         v_registers = [None] * 16
@@ -149,6 +166,7 @@ class Chip8Test(unittest.TestCase):
         self.assertEqual(expected_vx_value, actual_vx_value)
 
     def test_vx_and_vy(self):
+        """ 8XY2 """
         vx_index = 0x3
         vy_index = 0xF
         v_registers = [None] * 16
@@ -164,6 +182,7 @@ class Chip8Test(unittest.TestCase):
         self.assertEqual(expected_vx_value, actual_vx_value)
 
     def test_vx_xor_vy(self):
+        """ 8XY3 """
         vx_index = 0x4
         vy_index = 0x5
         v_registers = [None] * 16
@@ -177,3 +196,87 @@ class Chip8Test(unittest.TestCase):
         actual_vx_value = chip8.v_registers[vx_index]
 
         self.assertEqual(expected_vx_value, actual_vx_value)
+
+    def test_add_vy_to_vx(self):
+        """ 8XY4 """
+        pass
+
+    def test_subtract_vy_from_vx(self):
+        """ 8XY5 """
+        pass
+
+    def test_shift_right_vx(self):
+        """ 8XY6 """
+        pass
+
+    def test_subtract_vy_from_vx_negative(self):
+        """ 8XY7 """
+        pass
+
+    def test_shift_left_vx(self):
+        """ 8XYE """
+        pass
+
+    def test_skip_next_instruction_if_vx_not_equals_vy(self):
+        """ 9XY0 """
+        pass
+
+    def test_set_i_register(self):
+        """ ANNN """
+        pass
+
+    def test_jump_to_address_plus_v0(self):
+        """ BNNN """
+        pass
+
+    def test_set_vx_to_random_anded_value(self):
+        """ CXNN """
+        pass
+
+    def test_draw_sprite_at_coordinate(self):
+        """ DXYN """
+        pass
+
+    def test_skip_next_instruction_if_pressed_key_equals_vx(self):
+        """ EX9E """
+        pass
+
+    def test_skip_next_instruction_if_pressed_key_not_equals_vx(self):
+        """ EXA1 """
+        pass
+
+    def test_set_vx_to_delay_timer(self):
+        """ FX07"""
+        pass
+
+    def test_wait_for_key_store_value_in_vx(self):
+        """ FX0A """
+        pass
+
+    def test_set_delay_timer_to_vx(self):
+        """ FX15 """
+        pass
+
+    def test_set_sound_timer_to_vx(self):
+        """ FX18 """
+        pass
+
+    def test_add_vx_to_i_register(self):
+        """ FX1E """
+        pass
+
+    def test_set_i_register_to_location_of_sprite_for_vx(self):
+        """ FX29 """
+        pass
+
+    def test_store_bcd_of_vx_in_i_register(self):
+        """ FX33 """
+        pass
+
+    def test_store_from_v0_to_vx_in_i_register(self):
+        """ FX55 """
+        pass
+
+    def test_read_from_v0_to_vx_in_i_register(self):
+        """ Fx65"""
+        pass
