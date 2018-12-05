39d017f3908b8a5ec37209677be4cfb008af22a5
Julen Pardo <julen.pardo@outlook.es>
2018-12-05 12:49:24 +0100

    Test remaining opcode parser functions

--- tests/opcode_parser_test.py
+++ tests/opcode_parser_test.py
@@ -275,3 +275,267 @@ class OpcodeParserTest(unittest.TestCase):
         actual = opcode_parser.parse_operation_and_parameters(opcode)
 
         self.assertEqual(expected, actual)
+
+    def test_construct_operation__00e0(self):
+        opcode = bytes([0x00, 0xE0])
+        constant_part = '0e'
+
+        expected = '00e0'
+        actual = opcode_parser._construct_operation(opcode, constant_part)
+
+        self.assertEqual(expected, actual)
+
+    def test_construct_operation__00ee(self):
+        opcode = bytes([0x00, 0xEE])
+        constant_part = '0e'
+
+        expected = '00ee'
+        actual = opcode_parser._construct_operation(opcode, constant_part)
+
+        self.assertEqual(expected, actual)
+
+    def test_construct_operation__1nnn(self):
+        opcode = bytes([0x1C, 0xE4])
+        constant_part = 'nn'
+        last_nibble = 'n'
+
+        expected = '1nnn'
+        actual = opcode_parser._construct_operation(opcode, constant_part,
+                                                    last_nibble)
+
+        self.assertEqual(expected, actual)
+
+    def test_construct_operation__2nnn(self):
+        opcode = bytes([0x25, 0x36])
+        constant_part = 'nn'
+        last_nibble = 'n'
+
+        expected = '2nnn'
+        actual = opcode_parser._construct_operation(opcode, constant_part,
+                                                    last_nibble)
+
+        self.assertEqual(expected, actual)
+
+    def test_construct_operation__3xkk(self):
+        opcode = bytes([0x3D, 0x78])
+        constant_part = 'xk'
+        last_nibble = 'k'
+
+        expected = '3xkk'
+        actual = opcode_parser._construct_operation(opcode, constant_part,
+                                                    last_nibble)
+
+        self.assertEqual(expected, actual)
+
+    def test_construct_operation__4xkk(self):
+        opcode = bytes([0x41, 0xAE])
+
+        constant_part = 'xk'
+        last_nibble = 'k'
+
+        expected = '4xkk'
+        actual = opcode_parser._construct_operation(opcode, constant_part,
+                                                    last_nibble)
+
+        self.assertEqual(expected, actual)
+
+    def test_construct_operation__5xy0(self):
+        opcode = bytes([0x5D, 0x00])
+        constant_part = 'xy'
+        last_nibble = '0'
+
+        expected = '5xy0'
+        actual = opcode_parser._construct_operation(opcode, constant_part,
+                                                    last_nibble)
+
+        self.assertEqual(expected, actual)
+
+    def test_construct_operation__6xkk(self):
+        opcode = bytes([0x6A, 0x15])
+        constant_part = 'xk'
+        last_nibble = 'k'
+
+        expected = '6xkk'
+        actual = opcode_parser._construct_operation(opcode, constant_part,
+                                                    last_nibble)
+
+        self.assertEqual(expected, actual)
+
+    def test_construct_operation__7xkk(self):
+        opcode = bytes([0x71, 0xF5])
+        constant_part = 'xk'
+        last_nibble = 'k'
+
+        expected = '7xkk'
+        actual = opcode_parser._construct_operation(opcode, constant_part,
+                                                    last_nibble)
+
+        self.assertEqual(expected, actual)
+
+    def test_construct_operation__8xy0(self):
+        opcode = bytes([0x82, 0xC0])
+        constant_part = 'xy'
+        last_nibble = '0'
+
+        expected = '8xy0'
+        actual = opcode_parser._construct_operation(opcode, constant_part,
+                                                    last_nibble)
+
+        self.assertEqual(expected, actual)
+
+    def test_construct_operation__8xy1(self):
+        opcode = bytes([0x8A, 0x51])
+        constant_part = 'xy'
+
+        expected = '8xy1'
+        actual = opcode_parser._construct_operation(opcode, constant_part)
+
+        self.assertEqual(expected, actual)
+
+    def test_construct_operation__8xy2(self):
+        opcode = bytes([0x81, 0x02])
+        constant_part = 'xy'
+
+        expected = '8xy2'
+        actual = opcode_parser._construct_operation(opcode, constant_part)
+
+        self.assertEqual(expected, actual)
+
+    def test_construct_operation__8xy3(self):
+        opcode = bytes([0x8B, 0xB3])
+        constant_part = 'xy'
+
+        expected = '8xy3'
+        actual = opcode_parser._construct_operation(opcode, constant_part)
+
+        self.assertEqual(expected, actual)
+
+    def test_construct_operation__8xy4(self):
+        opcode = bytes([0x80, 0xC4])
+        constant_part = 'xy'
+
+        expected = '8xy4'
+        actual = opcode_parser._construct_operation(opcode, constant_part)
+
+        self.assertEqual(expected, actual)
+
+    def test_construct_operation__8xy5(self):
+        opcode = bytes([0x8F, 0xB5])
+        constant_part = 'xy'
+
+        expected = '8xy5'
+        actual = opcode_parser._construct_operation(opcode, constant_part)
+
+        self.assertEqual(expected, actual)
+
+    def test_construct_operation__8xy6(self):
+        opcode = bytes([0x82, 0x76])
+        constant_part = 'xy'
+
+        expected = '8xy6'
+        actual = opcode_parser._construct_operation(opcode, constant_part)
+
+        self.assertEqual(expected, actual)
+
+    def test_construct_operation__8xy7(self):
+        opcode = bytes([0x8E, 0x37])
+        constant_part = 'xy'
+
+        expected = '8xy7'
+        actual = opcode_parser._construct_operation(opcode, constant_part)
+
+        self.assertEqual(expected, actual)
+
+    def test_construct_operation__8xye(self):
+        opcode = bytes([0x8D, 0xCE])
+        constant_part = 'xy'
+
+        expected = '8xye'
+        actual = opcode_parser._construct_operation(opcode, constant_part)
+
+        self.assertEqual(expected, actual)
+
+    def test_construct_operation__9xy0(self):
+        opcode = bytes([0x92, 0x30])
+        constant_part = 'xy'
+
+        expected = '9xy0'
+        actual = opcode_parser._construct_operation(opcode, constant_part)
+
+        self.assertEqual(expected, actual)
+
+    def test_construct_operation__annn(self):
+        opcode = bytes([0xA7, 0xFE])
+        constant_part = 'nn'
+        last_nibble = 'n'
+
+        expected = 'annn'
+        actual = opcode_parser._construct_operation(opcode, constant_part,
+                                                   last_nibble)
+
+        self.assertEqual(expected, actual)
+
+    def test_construct_operation__bnnn(self):
+        opcode = bytes([0xB5, 0xB1])
+        constant_part = 'nn'
+        last_nibble = 'n'
+
+        expected = 'bnnn'
+        actual = opcode_parser._construct_operation(opcode, constant_part,
+                                                   last_nibble)
+
+        self.assertEqual(expected, actual)
+
+    def test_construct_operation__cxkk(self):
+        opcode = bytes([0xC1, 0x07])
+        constant_part = 'xk'
+        last_nibble = 'k'
+
+        expected = 'cxkk'
+        actual = opcode_parser._construct_operation(opcode, constant_part,
+                                                    last_nibble)
+
+        self.assertEqual(expected, actual)
+
+    def test_construct_operation__dxyn(self):
+        opcode = bytes([0xDF, 0x08])
+        constant_part = 'xy'
+        last_nibble = 'n'
+
+        expected = 'dxyn'
+        actual = opcode_parser._construct_operation(opcode, constant_part,
+                                                    last_nibble)
+
+        self.assertEqual(expected, actual)
+
+    def test_get_byte_first_nibble_int(self):
+        byte = 0xFD
+
+        expected = 0xF
+        actual = opcode_parser._get_byte_first_nibble_int(byte)
+
+        self.assertEqual(expected, actual)
+
+    def test_get_byte_last_nibble_int(self):
+        byte = 0xFD
+
+        expected = 0xD
+        actual = opcode_parser._get_byte_last_nibble_int(byte)
+
+        self.assertEqual(expected, actual)
+
+    def test_get_byte_first_nibble_hex(self):
+        byte = 0xFD
+
+        expected = 'f'
+        actual = opcode_parser._get_byte_first_nibble_hex(byte)
+
+        self.assertEqual(expected, actual)
+
+    def test_get_byte_last_nibble_hex(self):
+        byte = 0xFD
+
+        expected = 'd'
+        actual = opcode_parser._get_byte_last_nibble_hex(byte)
+
+        self.assertEqual(expected, actual)
